f0d7b547750bac7cde17654db64f05791c9f36d9
Kat Marchán <kzm@sykosomatic.org>
2009-11-28 01:12:49 -0600

    Tons of comments and a couple slight tweaks.

--- common-brick.lisp
+++ common-brick.lisp
@@ -13,14 +13,26 @@
 (defvar *resource-directory*
   (merge-pathnames "res/" (load-time-value (or #.*compile-file-truename* *load-truename*))))
 
+;;;
 ;;; Physics callback
-(defmessage collide-objects (a b contacts))
+;;;
+;;; - We use a custom collision callback because our game-objects are Sheeple-based. By default,
+;;;   SquirL uses a CLOS-based multi-dispatched genfun (COLLIDE), which this emulates.
+(defmessage collide-objects (a b contacts)
+  (:documentation "Collides A and B. If a reply returns NIL, the collision is not resolved by
+SquirL. Otherwise, the collision actually happens. The body of the reply is executed regardless."))
 
-;;; The engine object
+;;;
+;;; Engine
+;;;
+;;; - UID applications should define their own engine prototypes. This allows users
+;;;   to define some default behaviors, as well as move data that would otherwise be
+;;;   global into a convenient single object.
 (defproto =common-brick= (=engine=)
   ((uid:title "Common Brick")
    (uid:window-width 800)
    (uid:window-height 600)
+   (uid:clear-color uid:*white*)
    bricks
    paddles
    balls
@@ -33,59 +45,102 @@
     (map nil #'draw balls)))
 
 (defreply update ((game =common-brick=) dt &key)
-  (let ((fun (fun (update _ dt))))
+  (let ((update-fun (fun (update _ dt))))
     (with-properties (bricks paddles balls) game
-      (map nil fun paddles)
-      (map nil fun balls)
-      (map nil fun bricks))))
+      (map nil update-fun paddles)
+      (map nil update-fun balls)
+      (map nil update-fun bricks))))
 
+;;;
 ;;; Game objects
+;;;
+;;; - UID sprites do not hold position information, and squirl bodies do not hold graphics information.
+;;;   The clear solution is to just put them both into a "game object" which is what we will
+;;;   actually be interacting with.
 (defproto =game-object= ()
-  ((x 0) (y 0) graphic physics-body))
+  (graphic physics-body)
+  (:documentation "Common Brick game object."))
 
+;; A basic breakout game involves 3 "game objects": A bunch of bricks, one or more balls,
+;; and one or more paddles. We create prototypes for each of these 3 types. In this particular
+;; case, delegation makes sharing the image resource easy and transparent.
 (defproto =brick= (=game-object=)
   ((graphic (create-image (merge-pathnames "brick.png" *resource-directory*)))))
+;; For each prototype, we also need to provide an init-object reply that takes care of adding
+;; the actual physics body to each object. Each physics body has to be unique, so we can't just
+;; stuff it in the prototype.
 (defreply init-object :after ((obj =brick=) &key)
   (setf (physics-body obj)
+        ;; bricks are static bodies, so we don't provide a mass.
         (make-body :actor obj
                    :shapes (list (make-rectangle (width (graphic obj))
                                                  (height (graphic obj))
+                                                 ;; Restitution of 1 gives "perfect" bounce.
+                                                 ;; This'll make the ball(s) keep bouncing.
                                                  :restitution 1
+                                                 ;; Friction will make the ball rotate when
+                                                 ;; it strikes at an angle.
                                                  :friction 0.3)))))
 
+;; Rinse and repeat for the other object types...
 (defproto =paddle= (=game-object=)
   ((graphic (create-image (merge-pathnames "paddle.png" *resource-directory*)))))
 (defreply init-object :after ((obj =paddle=) &key)
   (let* ((width (width (graphic obj)))
          (height (height (graphic obj)))
+         ;; Note that width/height for UID images defaults to 1 unless the engine
+         ;; is already running, so this will only be accurate if OBJ is created
+         ;; inside a running engine.
          (point-a (vec (- (/ width 2)) 0))
          (point-b (vec (/ width 2) 0)))
     (setf (physics-body obj)
+          ;; The ACTOR slot in a body determines what object COLLIDE-OBJECTS will be called with
+          ;; when a collision between two shapes happens. In the case of our game objects,
+          ;; we want to write COLLIDE-OBJECTS replies that dispatch on the game objects themselves,
+          ;; so we set the physics body's actor to each object.
           (make-body :actor obj
                      :shapes (list (make-segment point-a point-b
                                                  :radius height
-                                                 :restitution 1.001
+                                                 ;; This should make it so balls speed up
+                                                 ;; slightly every time they hit a paddle
+                                                 ;; (since "bounce" is a little more than perfect)
+                                                 :restitution 1.01
                                                  :friction 0.6))))))
 
+;; And now our balls...
 (defproto =ball= (=game-object=)
   ((graphic (create-image (merge-pathnames "ball.png" *resource-directory*)))))
 (defreply init-object :after ((obj =ball=) &key)
   (let* ((radius (width (graphic obj))))
     (setf (physics-body obj)
           (make-body :actor obj
+                     ;; Balls are our only non-static objects, so they have some mass.
                      :mass 5
                      :shapes (list (make-circle radius :friction 0.3 :restitution 1))))))
 
+;; Now that we have our game objects, we write some boilerplate to update and draw them.
 (defreply draw ((object =game-object=) &rest args &key)
-  (with-properties (x y content) object
-    (apply 'draw-at x y content args)))
+  (with-properties (graphic physics-body) object
+    (let* ((vec (body-position physics-body))
+           (x (vec-x vec))
+           (y (vec-y vec)))
+      ;; todo: draw the rotation, too.
+      (apply 'draw-at x y graphic args))))
 
+;; If we ever feel like making a game object's GRAPHIC an animation or such, this reply
+;; will take care of updating it properly.
 (defreply update :before ((object =game-object=) dt &key)
   (update (graphic object) dt))
 
+;;;
 ;;; Game object collisions
-;; The reason we define functions separately is so that they can share a body and still close over
-;; the lexical environment properly. tl;dr: I'm anal.
+;;;
+;;; - SquirL has a DEFCOLLISION macro by default that abstracts away this nasty pattern.  Since
+;;;   we're using a custom callback, though, we need to write this out ourselves :(
+;;;
+;;;   The reason we define functions separately is so that they can share a body and still close
+;;;   over the lexical environment properly. tl;dr: I'm anal, and that's why this code is ugly.
+;;;
 (defun paddle/ball (paddle ball contacts)
   (declare (ignore paddle ball contacts))
   ;; for now, just return T, which means the collision happened.
